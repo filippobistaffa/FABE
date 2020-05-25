ce22619612322b3995a182f64a9c660c96a10c8c
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2020-05-25 14:15:41 +0200

    More intuitive quantisation

--- be.hpp
+++ be.hpp
@@ -4,8 +4,12 @@
 #include "types.hpp"
 #include <cfloat>
 
-#define QUANTISATION (1 / FLT_EPSILON)
-//#define QUANTISATION (1 / DBL_EPSILON)
+#define QUANTISATION 1e10
+//#define QUANTISATION 1e20
+//#define QUANTISATION 1e30
+//#define QUANTISATION 1e40
+//#define QUANTISATION 1e50
+//#define QUANTISATION 1e50
 
 vector<vector<automata>> compute_buckets(vector<automata> const &automatas, vector<size_t> const &pos);
 
